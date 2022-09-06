bdbd363e6061c4d4ed3b7c01438f326f5ed15ada
Tirasigor <31933244+Tirasigor@users.noreply.github.com>
2022-09-06 18:38:06 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,27 +1,32 @@
-let burger = document.querySelector('.header__burger');
+const burger = document.querySelector('.header__burger');
 const burgerMenu = document.querySelector('.header__nav-side');
-let overlay = document.querySelector('.overlay');
+const overlay = document.querySelector('.overlay');
 
 burger.addEventListener('click', (e) => {
+	console.log('click');
+	console.log(e.target);
 	e.preventDefault;
 	burger.classList.toggle('header__burger_active');
 	burgerMenu.classList.toggle('header__nav-side_active');
 	overlay.classList.toggle('overlay_active');
-
-	document.addEventListener('click', (e) => {
-		if (e.target !== burger)
-		{
-			burger.classList.remove('header__burger_active');
+});
+document.addEventListener('click', (e) => {
+	e.stopPropagation;
+	if (e.target !== burger) {
+		console.log('removeClick');
+		console.log(e.target);
+		burger.classList.remove('header__burger_active');
 		burgerMenu.classList.remove('header__nav-side_active');
 		overlay.classList.remove('overlay_active');
-		}
-	});
-	
-	document.addEventListener('keydown', function (e) {
-		if (e.key === 'Tab' || e.key === 'Escape') {
-			burger.classList.remove('header__burger_active');
-			burgerMenu.classList.remove('header__nav-side_active');
-			overlay.classList.remove('overlay_active');
-		}
-	});
+	}
+});
+
+document.addEventListener('keydown', function (e) {
+	if (e.key === 'Tab' || e.key === 'Escape') {
+		console.log('removeKey');
+		console.log(e.target);
+		burger.classList.remove('header__burger_active');
+		burgerMenu.classList.remove('header__nav-side_active');
+		overlay.classList.remove('overlay_active');
+	}
 });
